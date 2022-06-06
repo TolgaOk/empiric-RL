cf504dc03851853c774b59ba59a04177311e0666
Tolga OK <tolgaokk@gmail.com>
2022-06-06 12:14:20 +0300

    Add untested mujoco config

--- runs/mb/a2c/configs.py
+++ runs/mb/a2c/configs.py
@@ -231,8 +231,75 @@ AtariConfig = MBConfig(
     )
 )
 
+MujocoWalkerConfig = MBConfig(
+    policy=MBDenseActorCritic,
+    hyperparameters=dict(
+        lr=HyperParameter(
+            default=0.00025,
+            tune_fn=lambda trial: trial.suggest_loguniform("lr", 1e-5, 3e-3)),
+        n_steps=HyperParameter(
+            default=8,
+            tune_fn=lambda trial: trial.suggest_int("n_steps", 1, 16)),
+        n_envs=HyperParameter(
+            default=16,
+            tune_fn=None),
+        gae_lambda=HyperParameter(
+            default=0.95,
+            tune_fn=lambda trial: trial.suggest_uniform("gae_lambda", 0.5, 1.0)),
+        gamma=HyperParameter(
+            default=0.999,
+            tune_fn=lambda trial: trial.suggest_uniform("gamma", 0.95, 0.9999)),
+        ent_coef=HyperParameter(
+            default=1e-4,
+            tune_fn=lambda trial: trial.suggest_loguniform("ent_coef", 1e-5, 1e-2)),
+        vf_coef=HyperParameter(
+            default=0.5,
+            tune_fn=lambda trial: trial.suggest_uniform("vf_coef", 0.1, 1.0)),
+        max_grad_norm=HyperParameter(
+            default=0.5,
+            tune_fn=None),
+        total_timesteps=HyperParameter(
+            default=5000000,
+            tune_fn=None),
+        policy_kwargs=dict(
+            pi_layer_widths=HyperParameter(
+                default="[200, 300]",
+                tune_fn=lambda trial: trial.suggest_categorical(
+                    "pi_layer_widths",
+                    ["[128, 128]", "[64, 64, 64]", "[200, 300]", "[256, 256, 256]"]),
+                interpret=lambda choice: json.loads(choice)),
+            value_layer_widths=HyperParameter(
+                default="[256, 256, 256]",
+                tune_fn=lambda trial: trial.suggest_categorical(
+                    "value_layer_widths",
+                    ["[128, 128]", "[64, 64, 64]", "[200, 300]", "[256, 256, 256]"]),
+                interpret=lambda choice: json.loads(choice)),
+            pi_activation_fn=HyperParameter(
+                default="Tanh",
+                tune_fn=lambda trial: trial.suggest_categorical(
+                    "pi_activation_fn",
+                    ["ELU", "Tanh", "ReLU"]),
+                interpret=lambda choice: getattr(torch.nn, choice)),
+            value_activation_fn=HyperParameter(
+                default="ELU",
+                tune_fn=lambda trial: trial.suggest_categorical(
+                    "value_activation_fn",
+                    ["ELU", "Tanh", "ReLU"]),
+                interpret=lambda choice: getattr(torch.nn, choice)),
+        )
+    ),
+    gym_wrappers=[{"class": FloatObservation, "kwargs": {}}],
+    sb3_wrappers=[],
+    tuner=TunerInfo(
+        sampler_cls=optuna.samplers.TPESampler,
+        n_startup_trials=10,
+        direction="maximize",
+    )
+)
+
 all_configs = dict(
     LunarLander=LunarLanderConfig,
     BipedalWalker=BipedalWalkerConfig,
+    MujocoWalker=MujocoWalkerConfig,
     AtariEnv=AtariConfig,
 )
